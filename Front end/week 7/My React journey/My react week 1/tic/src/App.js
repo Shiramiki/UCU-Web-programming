f6fc7789514e7c3e72a9d0ed458a09590eeeda9a
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-03-01 14:43:31 +0300

    my first react app

--- Front end/week 7/My React journey/My react week 1/tic/src/App.js
+++ Front end/week 7/My React journey/My react week 1/tic/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import {useState} from 'react'
 
+// COMPONENT
+function Square({ value }) {
+  const[name, setName]=useState(null);
+
+  function handleButtonClick(){
+    setName("X")
+    console.log("Click action", value);
+  }
+  return <button className="square" onClick ={handleButtonClick}> {value}
+  {name}
+  </button>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="boardRow">
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
+      </div>
+
+      <div className="boardRow">
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
+      </div>
+
+      <div className="boardRow">
+        <Square value="" />
+        <Square value="" />
+        <Square value="" />
+      </div>
     </div>
   );
 }
